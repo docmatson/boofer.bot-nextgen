5d8c6f36bc7cae9543bb94934900b7c50e8b2126
Docmatson <docmatson@mars.gateway.sasktel.net>
2021-07-05 09:31:24 -0600

    fixed ping :)

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, msg, args) => {
 	msg.channel.send('Calculating...').then(tmsg =>{
-		tmsg.edit(`Latency to server is: ${tmsg.createdTimestamp - msg.createdTimestamp}ms. API Latency to server is: ${Math.round(client.ping)}ms`)
+		tmsg.edit(`Latency to server is: ${tmsg.createdTimestamp - msg.createdTimestamp}ms. API Latency to server is: ${Math.round(client.ws.ping)}ms`)
 	});
 }
 
